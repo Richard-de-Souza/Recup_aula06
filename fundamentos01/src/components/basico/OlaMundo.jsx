4574247c4ca3a3eb9dcda2bbaec93866a73a7592
Richard-de-Souza <rickpili22@gmail.com>
2021-10-06 14:36:58 +0000

    commit

---
+++ fundamentos01/src/components/basico/OlaMundo.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default  ()=>{
+    const informacao = 'primeiro componente JSX'
+    return (
+      <div>
+        <h2>componente OlaMundo</h2>
+        <p>{informacao}</p>
+      </div>
+    )
+}
